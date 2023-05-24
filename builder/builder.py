cb03d3c501314a23f5398d91f4146031e494776c
Ali Shahidi <alishahidi1376@gmail.com>
2023-05-24 22:20:18 +0330

    fix path in main readme

--- builder/builder.py
+++ builder/builder.py
@@ -24,9 +24,9 @@ for project in projects:
     project_directory = os.path.join(
         "projects", project_name.replace(" ", "_"))
     project_readme_path = os.path.join(project_directory, "README.md")
-    main_readme_content += f"- [{project_name}]({project_readme_path})\n"
+    main_readme_content += f"- [{project_name}]({project_readme_path})\n\n"
     first_project_image = project['images'][0]
-    main_readme_content += f"![{first_project_image['desc']}]({project['name']}/assets/{first_project_image['path']})\n"
+    main_readme_content += f"![{first_project_image['desc']}](projects/{project['name']}/assets/{first_project_image['path']})\n\n"
 
 # Write the project list to the main README.md file
 with open(main_readme_path, 'w') as main_readme_file:
@@ -47,7 +47,7 @@ for project in projects:
     readme_content += "## Screenshots\n\n"
 
     for image in project['images']:
-        readme_content += f"![{image['desc']}](assets/{image['path']})\n"
+        readme_content += f"![{image['desc']}](assets/{image['path']})\n\n"
         readme_content += f"**Description:** {image['desc']}\n\n"
     print(readme_path)
     # Write the README content to the file
