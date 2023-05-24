1705563b9e26d2ae6edcccf83cbe769dc5af2e9e
Ali Shahidi <alishahidi1376@gmail.com>
2023-05-24 22:13:22 +0330

    add builder and run it

---
+++ builder/builder.py
@@ -0,0 +1,56 @@
+import os
+import json
+
+# Path to the JSON file containing the projects array
+
+# Read the projects array from the JSON file
+with open("builder/content/projects.json", 'r') as json_file:
+    projects = json.load(json_file)
+
+with open("builder/content/main.json", 'r') as json_file:
+    main = json.load(json_file)
+
+# Update the main README.md file
+main_readme_path = "README.md"
+main_readme_content = f"""
+# {main["title"]}
+## {main["description"]}
+## Projects list
+"""
+
+# Generate the project list in the main README.md
+for project in projects:
+    project_name = project['name']
+    project_directory = project_name.replace(" ", "_")
+    project_readme_path = os.path.join(project_directory, "README.md")
+    main_readme_content += f"- [{project_name}]({project_readme_path})\n"
+    first_project_image = project['images'][0]
+    main_readme_content += f"![{project['name']}/{first_project_image['path']}]({project['name']}/{first_project_image['desc']})\n"
+
+# Write the project list to the main README.md file
+with open(main_readme_path, 'w') as main_readme_file:
+    main_readme_file.write(main_readme_content)
+
+# Create/update the README.md file for each project
+for project in projects:
+    project_name = project['name']
+    project_directory = os.path.join(
+        "projects", project_name.replace(" ", "_"))
+    readme_path = os.path.join(project_directory, "README.md")
+
+    # Generate the README content
+    readme_content = f"# {project_name}\n\n"
+    readme_content += f"**Used Technology:** {', '.join(project['technologies'])}\n\n"
+    readme_content += f"**Project Link:** [{project_name}]({project['link']})\n\n"
+    readme_content += f"**Project Website:** [{project_name} Website]({project['website']})\n\n"
+    readme_content += "## Screenshots\n\n"
+
+    for image in project['images']:
+        readme_content += f"![{image['path']}]({image['desc']})\n"
+        readme_content += f"**Description:** {image['desc']}\n\n"
+    print(readme_path)
+    # Write the README content to the file
+    with open(readme_path, 'w') as readme_file:
+        readme_file.write(readme_content)
+
+print("README.md files updated successfully.")
---
+++ projects/alishahidinet/README.md
@@ -0,0 +1,25 @@
+# alishahidinet
+
+**Used Technology:** php, bootstrap, mysql, html css
+
+**Project Link:** [alishahidinet](https://gtihub.com/alishahidi/alishahidinet)
+
+**Project Website:** [alishahidinet Website](https://alishahidinet.ir)
+
+## Screenshots
+
+![1.jpg](Menu bar)
+**Description:** Menu bar
+
+![2.jpg](Menu bar 1)
+**Description:** Menu bar 1
+
+![3.jpg](Menu bar 2)
+**Description:** Menu bar 2
+
+![4.jpg](Menu bar 3)
+**Description:** Menu bar 3
+
+![5.jpg](Menu bar 4)
+**Description:** Menu bar 4
+
